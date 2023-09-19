e1716d0e2c1c83b0bffa5ea1c39215ce5a6bbf2b
akash <md.mostafa.akash@gmail.com>
2023-09-19 16:31:40 +0600

    Parsing request

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -25,8 +27,29 @@ app.createServer = () => {
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello Mostafa!');
+    // Request handling
+    // Parsing the url
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObj = parsedUrl.query;
+    const headersObj = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        console.log(realData);
+        // response handle
+        res.end('Hello Mostafa!');
+    });
 };
 
 // Start the server
