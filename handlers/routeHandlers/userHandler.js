49fa28e7f41d18069cdefca9708ff79744b77903
akash <md.mostafa.akash@gmail.com>
2023-09-20 15:06:44 +0600

    Update user

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -92,7 +92,57 @@ handler._users.get = (requestProperties, callBack) => {
 };
 
 handler._users.put = (requestProperties, callBack) => {
+    const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
+    const lastName = typeof requestProperties.body.lastName === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
+    const phone = typeof requestProperties.body.phone === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
+    const password = typeof requestProperties.body.password === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
 
+    if (phone) {
+        if (firstName || lastName || password) {
+            // lookup the user
+            data.read('users', phone, (err, uData) => {
+                const userData = { ...parseJSON(uData) };
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    // store to database
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callBack(200, {
+                                message: 'User was updated successfully',
+                            });
+                        } else {
+                            callBack(500, {
+                                error: 'There was a problem in the server side!',
+                            });
+                        }
+                    });
+                } else {
+                    callBack(400, {
+                        error: 'You have a problem in your request!',
+                    });
+                }
+            });
+        } else {
+            callBack(400, {
+                error: 'You have a problem in your request!',
+            });
+        }
+    } else {
+        callBack(404, {
+            error: 'Invalid phone number. Please try again!',
+        });
+    }
 };
 
 handler._users.delete = (requestProperties, callBack) => {
