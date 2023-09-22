d49f0b5ff739c6cb9de4530b153d0acfb87435b6
akash <md.mostafa.akash@gmail.com>
2023-09-23 00:51:53 +0600

    Check put request

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -155,12 +155,89 @@ handler._check.get = (requestProperties, callback) => {
     }
 };
 
-// handler._check.put = (requestProperties, callBack) => {
-//     const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
-//     const lastName = typeof requestProperties.body.lastName === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
-//     const phone = typeof requestProperties.body.phone === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
-//     const password = typeof requestProperties.body.password === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
-// };
+handler._check.put = (requestProperties, callback) => {
+    // validate inputs
+    const id = typeof requestProperties.body.id === 'string' && requestProperties.body.id.trim().length === 20 ? requestProperties.body.id : false;
+
+    const protocol = typeof requestProperties.body.protocol === 'string'
+     && ['http', 'https'].indexOf(requestProperties.body.protocol) > -1
+         ? requestProperties.body.protocol
+         : false;
+
+    const url = typeof requestProperties.body.url === 'string'
+        && requestProperties.body.url.trim().length > 0
+            ? requestProperties.body.url
+            : false;
+
+    const method = typeof requestProperties.body.method === 'string'
+        && ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1
+            ? requestProperties.body.method
+            : false;
+
+    const successCodes = typeof requestProperties.body.successCodes === 'object'
+        && requestProperties.body.successCodes instanceof Array
+            ? requestProperties.body.successCodes
+            : false;
+
+    const timeoutSeconds = typeof requestProperties.body.timeoutSeconds === 'number'
+        && requestProperties.body.timeoutSeconds % 1 === 0
+        && requestProperties.body.timeoutSeconds >= 1
+        && requestProperties.body.timeoutSeconds <= 5
+            ? requestProperties.body.timeoutSeconds
+            : false;
+
+    if (id) {
+        if (protocol || url || method || successCodes || timeoutSeconds) {
+            data.read('checks', id, (err1, checkData) => {
+                if (!err1 && checkData) {
+                    const checkObject = parseJSON(checkData);
+                    const token = typeof requestProperties.headersObject.token === 'string' ? requestProperties.headersObject.token : false;
+
+                    tokenHandler._token.verify(token, checkObject.userPhone, (tokenIsValid) => {
+                        if (tokenIsValid) {
+                            if (protocol) {
+                                checkObject.protocol = protocol;
+                            }
+
+                            if (url) {
+                                checkObject.url = url;
+                            }
+
+                            if (method) {
+                                checkObject.method = method;
+                            }
+
+                            if (successCodes) {
+                                checkObject.successCodes = successCodes;
+                            }
+
+                            if (timeoutSeconds) {
+                                checkObject.timeoutSeconds = timeoutSeconds;
+                            }
+
+                            // store the checkObject
+                            data.update('checks', id, checkObject, (err2) => {
+                                if (!err2) {
+                                    callback(200);
+                                } else {
+                                    callback(500, { error: 'There was a server side error!' });
+                                }
+                            });
+                        } else {
+                            callback(403, { error: 'Authentication failure!' });
+                        }
+                    });
+                } else {
+                    callback(500, { error: 'There was a problem in the server side!' });
+                }
+            });
+        } else {
+            callback(400, { error: 'You must provide at least one field to update!' });
+        }
+    } else {
+        callback(400, { error: 'You have a problem in your request!' });
+    }
+};
 
 // handler._check.delete = (requestProperties, callBack) => {
 //     const phone = typeof requestProperties.queryStringObject.phone === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
